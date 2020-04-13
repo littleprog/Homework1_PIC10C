b0c78478559e41d9dacae164d0e42ba14d7e17f3
Seng Chow <sengchowtech@gmail.com>
2020-04-12 21:59:24 -0700

    Original Code

---
+++ Homework1_PIC10C/Homework1_PIC10C/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  Homework1_PIC10C
+//
+//  Created by Seng chow Choy on 4/12/20.
+//  Copyright © 2020 Seng chow Choy. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+
+using namespace std;
+
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main(){
+    /* --STATEMENTS-- */
+    return 0;
+}
